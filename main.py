c4fe6a574dc8a3ed98e9e464649028e55339f20c
Akilesh Mahesh <akilesh.mahesh07@gmail.com>
2024-07-05 15:24:28 -0400

    API Integration

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 import cred
+import google.generativeai as genai
 
-api_key = cred.google_palm_api_key
+
+genai.configure(api_key = cred.google_palm_api_key)
+
+#or m in genai.list_models():
+#    if 'generateContent' in m.supported_generation_methods:
+#        print(m.name)
+
+
+model = genai.GenerativeModel('gemini-1.0-pro-latest')
+
+
+prompt = input("Enter your prompt: ")
+response = model.generate_content(prompt)
+print(response.text)
